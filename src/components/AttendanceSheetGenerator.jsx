3e52be6954c98fed79d1d84af2fd549abc57eb9a
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2024-10-06 19:33:13 +0600

    table title formatting

--- src/components/AttendanceSheetGenerator.jsx
+++ src/components/AttendanceSheetGenerator.jsx
@@ -394,8 +394,8 @@ Roll: 1,2,3,4,5,....
                         {/* Show Table */}
                         {showTable && (
                             <>
+                                <h2 className="text-xl text-start font-bold mb-4">Attendance Table : </h2>
                                 <div className="mt-4 overflow-x-auto">
-                                    <h2 className="text-xl text-start font-bold mb-4">Attendance Table : </h2>
                                     <table className="table-auto border-collapse border border-gray-400 w-full">
                                         <thead>
                                             <tr className="bg-blue-600 text-white">
